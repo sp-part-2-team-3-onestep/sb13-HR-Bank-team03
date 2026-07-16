c85632c60ea7bb6db0a27e86b943a509148aefe9
ssummer96 <pupu25866@gmail.com>
2026-07-16 09:22:02 +0900

    debug-findById누락 수정

--- src/main/java/com/project/hrbank/service/EmployeeService.java
+++ src/main/java/com/project/hrbank/service/EmployeeService.java
@@ -15,6 +15,8 @@ import java.util.List;
 
 public interface EmployeeService {
 
+    EmployeeDto findById(Long id);
+
     long countEmployees(
             EmployeeStatus status,
             LocalDate fromDate,
